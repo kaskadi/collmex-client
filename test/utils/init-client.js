26007c789b43581aad5c7e59c0d6946a22635733
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-01 14:06:16 +0200

    Removed init-client util

--- test/utils/init-client.js
+++
@@ -1,9 +0,0 @@
-module.exports = () => {
-  let options = {}
-  try {
-    options = require('../data/options.json')
-  } catch (err) {
-    options = require('../data/options.default.json')
-  }
-  return require('../../index.js')(options)
-}
