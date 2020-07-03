059c6861eaa165052bdfe1ce92ea21e4838563ee
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-03 18:32:06 +0200

    Added satzarten loading helper with fallback

--- modules/parse-data.js
+++ modules/parse-data.js
@@ -1,5 +1,5 @@
 module.exports = (data) => {
-  const satzarten = require('./satzarten.json')
+  const satzarten = require('./load-satzarten.js')()
   return data.map(row => {
     const satz = satzarten[row[0]]
     return Object.fromEntries(Object.keys(satz).map((key, index) => [key, row[index] || '']))
