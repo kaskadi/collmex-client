a8d16caca513d965a38ae147ea0867a06c557b61
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-07-03 18:33:04 +0200

    Fixed typo in fallback path

--- modules/load-satzarten.js
+++ modules/load-satzarten.js
@@ -1,6 +1,6 @@
 module.exports = () => {
   const path = `${process.cwd()}/data/satzarten.json`
   const { existsSync } = require('fs')
-  const fallbackPath = `${path.slice(0, -5)}.originalS${path.slice(-5)}`
+  const fallbackPath = `${path.slice(0, -5)}.original${path.slice(-5)}`
   return existsSync(path) ? require(path) : require(fallbackPath)
 }
