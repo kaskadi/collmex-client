e1b2ec2a244c9dbb76be28f8db3c05ed8305cd6e
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-18 18:40:01 +0200

    Fixing docs

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const sanitizeData = require('./modules/sanitize-data.js')
 const parseData = require('./modules/parse-data.js')
 
 /**
+ * Options for the new Collmex client instanciation
+ * @typedef {Object} Options
+ * @property {string} User - Collmex user
+ * @property {string} Password - Collmex password for given user
+ * @property {number} CMXKundennummer - Collmex Customer Number
+ * @property {number} Firma_Nr - Company Number (as registered with Collmex)
+ * @property {string} [Systemname=collmex-client] - User-agent you would like to use for your client
+ * @property {string} [Output=object] - Desired output type. Will be set for any further `get` calls except if reassigned or overwritten via `get` `output_format` parameter.
+ */
+
+/**
  *
  * Creates a new client to communicate with Collmex API.
  *
  * @module collmex-client
  * @typicalname collmex
- * @param {Options} opts - {@link collmex-client:options|Options} to be passed to instanciate a new client
+ * @param {Options} opts - Options to be passed to instanciate a new client
  * @returns {Collmex} Collmex client
  *
  * @example
@@ -21,17 +32,6 @@ const parseData = require('./modules/parse-data.js')
  *   "Firma_Nr" : 1,
  *   "Systemname" : "collmex-test"
  * })
- */
-
-/**
- * Options for the new Collmex client instanciation
- * @typedef {Object} Options
- * @property {string} User - Collmex user
- * @property {string} Password - Collmex password for given user
- * @property {number} CMXKundennummer - Collmex Customer Number
- * @property {number} Firma_Nr - Company Number (as registered with Collmex)
- * @property {string} [Systemname=collmex-client] - User-agent you would like to use for your client
- * @property {string} [Output=object] - Desired output type. Will be set for any further `get` calls except if reassigned or overwritten via `get` `output_format` parameter.
  */
 
 class Collmex {
@@ -45,7 +45,7 @@ class Collmex {
   }
 
   /**
-   * Use this method to call Collmex API.
+   * Calls Collmex API.
    * @function module:collmex-client.get
    * @param {object|object[]} data - Data for the request to send to Collmex. Use an `array` if you would like to send multiple requests at once.
    * @param {string} [output_format=object] - Desired output type (set only for that specific call). See [here]{@link #output-formats} for `output_format` valid values
