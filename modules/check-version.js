6acfded1173228e5e298e62c779d17ddae046948
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-12-02 12:33:28 +0100

    Added utility to compare local/remote versions

---
+++ modules/check-version.js
@@ -0,0 +1,22 @@
+const { readFileSync } = require('fs')
+const path = require('path')
+const { spawnSync } = require('child_process')
+
+module.exports = () => {
+  const pjson = JSON.parse(readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'))
+  const pkgName = pjson.name
+  console.log(`INFO: comparing locally installed version of ${pkgName} to latest available on npm...`)
+  const localVer = pjson.version
+  const npmData = JSON.parse(spawnSync('npm', ['view', pkgName, '--json']).stdout.toString())
+  const remoteVer = npmData.version
+  if (checkSum(localVer) < checkSum(remoteVer)) {
+    console.log(`WARNING: your local version of ${pkgName} is ${localVer} while the latest available version on npm is ${remoteVer}. Please consider updating your client as you may be using outdated CSV mapping...`)
+  }
+}
+
+function checkSum (ver) {
+  // here we do a weighted check sum where major version has a weight of 100, minor has 10 and patch has 1. We disregard the beta/alpha flags as we probably won't be using them
+  return ver
+    .split('.')
+    .reduce((acc, cur, i) => acc + (10 ** (2 - i)) * cur, 0)
+}
